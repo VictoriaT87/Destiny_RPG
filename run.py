1872f7f22713c767b01207b0b15eb162d0317967
VictoriaT87 <victoriaemt@gmail.com>
2022-11-11 14:28:33 +0000

    Add get_class function to allow player to choose their character class

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def get_name():
     print("I've been searching for you for centuries.\n")
     print("What should I call you?")
     while True:
-        name = input("\n> ")
+        name = input("\n> ").capitalize()
         # https://www.w3schools.com/python/ref_string_isalpha.asp
         if not name.isalpha():
             print("Please enter letters only.")
@@ -21,4 +21,27 @@ def get_name():
             break
     return name
 
+
+def get_class():
+    """
+    Player chooses their class. 3 available based on Destiny lore.
+    """
+    print("Let's try to figure out what kind of Guardian you are... \n")
+    print("Do you think you're a Hunter, Warlock or Titan?\n")
+    print("Hunter: Agile and daring, Hunters are quick on their feet and quicker on the draw.\n")
+    print("Warlock: Warlocks weaponize the mysteries of the universe to sustain themselves and devastate their foes.\n")
+    print("Titan: Disciplined and proud, Titans are capable of both aggresive assaults and stalwart defenses.\n")
+    while True:
+        chosen_class = input("My class is: ").capitalize()
+        classes = ["Hunter", "Warlock", "Titan"]
+        if chosen_class in classes:
+            print(f"Welcome, {chosen_class}.")
+            break
+        else:
+            print("Please type one of the classes listed.")
+            continue
+    return chosen_class
+
+
 get_name()
+get_class()
