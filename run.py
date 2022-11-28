4b65390e52d00b13d21b6c0b1b31f71ac3e72a07
VictoriaT87 <victoriaemt@gmail.com>
2022-11-28 15:08:42 +0000

    Move game functions to top of run.py, fix play_again method - line 108

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import random
-# import time
+import time
 import pyfiglet
 
 import gspread
@@ -22,15 +22,100 @@ class_worksheet = SHEET.worksheet('PlayerClass')
 stats_worksheet = SHEET.worksheet('PlayerStats')
 
 
-# def slow_text(text):
-#     """
-#     Slows the speed of the text being printed.
-#     https://stackoverflow.com/questions/60608275/how-can-i-print-text-so-it-looks-like-its-being-typed-out
-#     """
-#     text += "\n"
-#     for char in text:
-#         time.sleep(0.09)
-#         print(char, end="", flush=True)
+class GameFunctions:
+    """
+    Class for all game functions
+    """
+    def reset_console(self):
+        """
+        Reset the console for a new game or continue.
+        """
+        print("\n")
+        os.system('cls||clear')
+
+    def play_again(self):
+        """
+        Option to allow player to play again or exit
+        """
+        print("Yes or No?")
+        user_input = ""
+        user_input = input("\n> ").capitalize()
+        if user_input == "Yes":
+            GameFunctions.reset_console(self)
+            new_story.introduction()
+        elif user_input == "No":
+            print("Thank you for playing, Guardian!")
+            self.clear_worksheet()
+            sys.exit()
+        else:
+            print("Please enter Yes or No.")
+
+    def clear_worksheet(self):
+        """
+        Clear player spreadsheet at start of game
+        """
+        class_worksheet.delete_rows(2)
+        stats_worksheet.delete_rows(2)
+
+    def inital_luck(self):
+        """Roll an Inital Luck number for the character"""
+        character_luck = random.randint(0, 100)
+        stats_worksheet.update_cell(2, 1, character_luck)
+        return character_luck
+
+    def check_items(self):
+        """
+        Function to check if the player has an key in their inventory
+        """
+        item_find = random.choice([True, False])
+        if item_find is True:
+            guardian.items.append("key")
+            print("You've found a key!")
+
+    def check_weapon(self):
+        """
+        Function to choose if the player gets a random weapoon from the chest
+        """
+        weapon_find = random.choice([True, False])
+        if weapon_find is True:
+            weapon = random.choice([
+                                    "Zhalo Supercell",
+                                    "The Last Word",
+                                    "No Land Beyond",
+                                    "Felwinter's Lie",
+                                    "Gjallarhorn"
+                                    ])
+            class_worksheet.update_cell(2, 4, weapon)
+            print(f"You've found a {weapon}!")
+            return weapon
+
+        elif weapon_find is False:
+            print("There was nothing in the chest, only dust...")
+
+    def handle_vandal(self):
+        """
+        function for random Vandal encounter
+        """
+        vandal_attack = random.choice([True, False])
+        if vandal_attack is True:
+            print("Out of nowhere, a Fallen Vandal attacks you!")
+            print("You took some damage :(")
+            guardian.health -= random.randint(1, 100)
+            print(f"\nHealth: {guardian.health}")
+            if guardian.health < 0:
+                print("You are dead!")
+                print("Your Ghost can ressurect you. Do you want him to?")
+                GameFunctions.play_again(self)
+
+    def slow_text(self, text):
+        """
+        Slows the speed of the text being printed.
+        https://stackoverflow.com/questions/60608275/how-can-i-print-text-so-it-looks-like-its-being-typed-out
+        """
+        text += "\n"
+        for char in text:
+            time.sleep(0.09)
+            print(char, end="", flush=True)
 
 
 class Player:
@@ -471,92 +556,6 @@ class Story:
             print("Thanks for playing, Guardian!")
             print("Would you like to play again?")
             GameFunctions.play_again(self)
-
-
-class GameFunctions:
-    """
-    Class for all game functions
-    """
-    def reset_console(self):
-        """
-        Reset the console for a new game or continue.
-        """
-        print("\n")
-        os.system('cls||clear')
-
-    def play_again(self):
-        """
-        Option to allow player to play again or exit
-        """
-        print("Yes or No?")
-        user_input = ""
-        user_input = input("\n> ").capitalize()
-        if user_input == "Yes":
-            self.reset_console()
-            new_story.introduction()
-        elif user_input == "No":
-            print("Thank you for playing, Guardian!")
-            self.clear_worksheet()
-            sys.exit()
-        else:
-            print("Please enter Yes or No.")
-
-    def clear_worksheet(self):
-        """
-        Clear player spreadsheet at start of game
-        """
-        class_worksheet.delete_rows(2)
-        stats_worksheet.delete_rows(2)
-
-    def inital_luck(self):
-        """Roll an Inital Luck number for the character"""
-        character_luck = random.randint(0, 100)
-        stats_worksheet.update_cell(2, 1, character_luck)
-        return character_luck
-
-    def check_items(self):
-        """
-        Function to check if the player has an key in their inventory
-        """
-        item_find = random.choice([True, False])
-        if item_find is True:
-            guardian.items.append("key")
-            print("You've found a key!")
-
-    def check_weapon(self):
-        """
-        Function to choose if the player gets a random weapoon from the chest
-        """
-        weapon_find = random.choice([True, False])
-        if weapon_find is True:
-            weapon = random.choice([
-                                    "Zhalo Supercell",
-                                    "The Last Word",
-                                    "No Land Beyond",
-                                    "Felwinter's Lie",
-                                    "Gjallarhorn"
-                                    ])
-            class_worksheet.update_cell(2, 4, weapon)
-            print(f"You've found a {weapon}!")
-            return weapon
-
-        elif weapon_find is False:
-            print("There was nothing in the chest, only dust...")
-
-    def handle_vandal(self):
-        """
-        function for random Vandal encounter
-        """
-        vandal_attack = random.choice([True, False])
-        if vandal_attack is True:
-            print("Out of nowhere, a Fallen Vandal attacks you!")
-            print("You took some damage :(")
-            guardian.health -= random.randint(1, 100)
-            print(f"\nHealth: {guardian.health}")
-            if guardian.health < 0:
-                print("You are dead!")
-                print("Your Ghost can ressurect you. Do you want him to?")
-                self.play_again()
 
 
 new_story = Story()
