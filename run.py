e023790e3909fa5dfa082fb8d3d9e33a8f21563c
VictoriaT87 <victoriaemt@gmail.com>
2022-12-02 18:40:56 +0000

    Add maximum length for username in get_name()

--- run.py
+++ run.py
@@ -209,8 +209,8 @@ class Story:
             # https://www.w3schools.com/python/ref_string_isalpha.asp
             if not name.isalpha():
                 print("Please enter letters only.")
-            elif len(name.strip(" ")) < 3:
-                print("Please enter a name at least 3 letters long.")
+            elif len(name.strip(" ")) < 3 or len(name.strip(" ")) > 8:
+                print("Please enter a name btween 3 and 8 letters long.")
             else:
                 function.s_print(f"It's nice to meet you, {name}."
                                  " I'm your Ghost.")
